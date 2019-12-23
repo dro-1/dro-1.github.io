a0e36d138bfb4cc16f99bf04361fbf3e7814817c
Taiwo Seun <lorddro1532@gmail.com>
2019-12-24 00:45:16 +0100

    Website Upload

---
+++ script_background.js
@@ -0,0 +1,46 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+const setBackground = () => {
+    let body = document.querySelector("body");
+    let css = document.querySelector("#css");
+    body.style.background = "linear-gradient( to right, "+color1.value + ", " +color2.value;
+    css.textContent = body.style.background+";";
+}
+
+const genRandomHex = () => {
+var rand;
+    do{
+        rand = Math.random()*100000000;
+    }
+    while(rand>16777215);
+    
+    rand = Math.round(rand);
+    rand = rand.toString(16);
+    rand = rand + "";
+    
+if(rand.length <6){
+        var zeroes = 6 - (rand.length);
+        while(zeroes>0){
+        rand = "0" + rand;
+        --zeroes;
+        }
+    }
+    return rand;
+}
+
+const randomizeColors = () => {
+    color1.value = "#" + genRandomHex();
+    color2.value = "#" + genRandomHex();
+}
+
+setBackground();
+
+var button = document.querySelector("button");
+button.addEventListener("click",randomizeColors);
+button.addEventListener("click",setBackground);
+
+color1.addEventListener("input",setBackground);
+
+color2.addEventListener("input",setBackground);
+
